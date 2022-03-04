56e2600f27c73eacaf153b0e77ecbdd30c89cd5d
EashanKaushik <Eashank16@gmail.com>
2022-03-04 17:07:52 -0500

    postgres fix

--- swe1_app/settings.py
+++ swe1_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '4g$@lx^xdpp($$7+5i=$s(e3o@4z=rj=53j!wws+3@7)r8(ysa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
